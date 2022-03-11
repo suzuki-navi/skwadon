1d9f1adbe2adff72851ff8e7627531c046f59570
suzuki-navi <suzuki.navi.dev@gmail.com>
2022-03-11 11:40:07 +0900

    implement glue.connections.*.connection

--- skwadon/aws_glue_connection.py
+++ skwadon/aws_glue_connection.py
@@ -1,5 +1,6 @@
 import copy
 import json
+import re
 
 import skwadon.main as sic_main
 import skwadon.lib as sic_lib
@@ -25,7 +26,7 @@ class ConnectionListHandler(common_action.ListHandler):
     def child_handler(self, name):
         return common_action.NamespaceHandler({
             "conf": ConnectionConfHandler(self.glue_client, name),
-            "connection": ConnectionConfHandler(self.glue_client, name),
+            "connection": ConnectionConnectionHandler(self.glue_client, name),
         })
 
 class ConnectionConfHandler(common_action.ResourceHandler):
@@ -61,3 +62,43 @@ class ConnectionConfHandler(common_action.ResourceHandler):
             sic_main.add_update_message(f"glue_client.update_connection(Name = {self.connection_name}, ...)")
             if confirmation_flag and sic_main.global_confirmation_flag:
                 self.glue_client.update_connection(Name = self.connection_name, ConnectionInput = update_data)
+
+class ConnectionConnectionHandler(common_action.ResourceHandler):
+
+    properties = [
+        "Description",
+        "ConnectionType",
+        "MatchCriteria",
+        "ConnectionProperties",
+        "PhysicalConnectionRequirements",
+    ]
+
+    def __init__(self, glue_client, connection_name):
+        self.glue_client = glue_client
+        self.connection_name = connection_name
+
+    def describe(self):
+        res = self.glue_client.get_connection(Name = self.connection_name)
+        jdbc_url = sic_lib.dictlib_get(res, "Connection.ConnectionProperties.JDBC_CONNECTION_URL")
+        curr_data = {
+        }
+        if jdbc_url != None:
+            curr_data["JBCDUrl"] = jdbc_url
+            m = re.compile("\Ajdbc:([^:]+)://([^:]+):([0-9]+)/(.+)\Z").search(jdbc_url)
+            if m:
+                driver = m.group(1)
+                if driver == "postgresql" or driver == "redshift":
+                    host = m.group(2)
+                    port = m.group(3)
+                    db_name = m.group(4)
+                    user_name = sic_lib.dictlib_get(res, "Connection.ConnectionProperties.USERNAME")
+                    password = sic_lib.dictlib_get(res, "Connection.ConnectionProperties.PASSWORD")
+                    cmd = f"PGPASSWORD='{password}' psql -h {host} -p {port} -U {user_name} -d {db_name}"
+                    curr_data["CommandLine"] = cmd
+        return curr_data
+
+    def create(self, confirmation_flag, src_data):
+        pass
+
+    def update(self, confirmation_flag, src_data, curr_data):
+        pass
