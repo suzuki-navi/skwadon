c189669f071f93e512d4f5889e48c1382b200d49
suzuki-navi <suzuki.navi.dev@gmail.com>
2022-06-11 23:53:34 +0900

    update test script

--- test/README.md
+++ test/README.md
@@ -6,3 +6,5 @@ $ bash ./test/test.sh
 testディレクトリの中の *.txt が1つずつテストケースになっている。
 test/test.sh を実行すると、その中にあるすべてが実行される。
 
+awsは --profile skwadon-test というプロファイルが使用される。
+
--- test/test.sh
+++ test/test.sh
@@ -2,17 +2,30 @@
 set -eu
 set -o pipefail
 
+basedir=$(dirname $0)/..
+
+targets=()
+while [ "$#" != 0 ]; do
+    targets+=("$1")
+    shift
+done
+if [ "${#targets[@]}" = 0 ]; then
+    cd $(dirname $0)
+    targets=(testcloud aws)
+    basedir=..
+fi
+
 check() {
     filepath=$1
     code=$(cat $filepath| (grep '^\$ ' || true) | cut -b3- | head -n1)
     echo -e "\e[34m$filepath\e[0m"
-    cat $filepath | (grep '^< ' || true) | cut -b3- > var/input.txt
-    cat $filepath | (grep '^> ' || true) | cut -b3- > var/expected.txt
+    cat $filepath | (grep '^< ' || true) | cut -b3- > $basedir/var/input.txt
+    cat $filepath | (grep '^> ' || true) | cut -b3- > $basedir/var/expected.txt
     cat $filepath
-    $code | strip_diff > var/actual.txt
-    if ! diff -u var/expected.txt var/actual.txt > /dev/null; then
+    (cd $basedir; $code) | strip_diff > $basedir/var/actual.txt
+    if ! diff -u $basedir/var/expected.txt $basedir/var/actual.txt > /dev/null; then
         echo
-        cat var/actual.txt | sed -e 's/^/> /g'
+        cat $basedir/var/actual.txt | sed -e 's/^/> /g'
         echo -e "\e[31mNG\e[0m"
         echo "see: diff var/actual.txt var/expected.txt"
         echo
@@ -27,24 +40,18 @@ strip_diff() {
 }
 
 r=0
-if [ "$#" = 0 ]; then
-    for f in $(ls test/*.txt); do
+for dir in ${targets[@]}; do
+    for f in $(find $dir -type f -name '*.txt' | sort); do
         check $f || exit 1
     done
-else
-    while [ "$#" != 0 ]; do
-        f=$1
-        shift
-        check $f || r=1
-    done
-fi
+done
 
 #HHMM=$(TZ= date +%H%M)
 
 if [ "$r" = 0 ]; then
-    echo All OK
+    echo -e "\e[32mAll OK\e[0m"
 else
-    echo NG
+    echo -e "\e[31mNG\e[0m"
     exit 1
 fi
 
